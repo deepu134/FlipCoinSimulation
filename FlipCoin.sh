a6aa105f324c1eef9f942691d9a4d6c840aa1caa
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-09 21:35:25 +0100

    rewrite

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -x
 echo "FLIP COIN SIMULATION PROBLEM"
 read -p "ENTER THE NUMBER OF FLIPS" num
+count=0
 for (( i=0;i<$num;i++ ))
 do
 	rand=$((RANDOM%2))
 	if [ $rand -eq 1 ]
 	then
 		echo "HEADS"
+		((countH++))
 	else
 		echo "TAIL"
+		((countT++))
 	fi
 done
+echo "HEADS COUNT:$countH TAIL COUNT:$countT"

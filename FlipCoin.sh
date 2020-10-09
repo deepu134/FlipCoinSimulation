39ecb3cc51070f2affd7af3859d60c0e24c73641
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-09 21:22:16 +0100

    Added:Find max win on 21 flips

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,13 +1,24 @@
 #!/bin/bash -x
 echo "FLIP COIN SIMULATION PROBLEM"
-read -p "ENTER THE NUMBER OF FLIPS" num
-for (( i=0;i<$num;i++ ))
+countH=0
+countT=0
+for ((i=0;i<21;i++))
 do
 	rand=$((RANDOM%2))
-	if [ $rand -eq 1 ]
-	then
-		echo "HEADS"
-	else
-		echo "TAIL"
-	fi
+	case $rand in
+		0)
+		((countH++));;
+		1)
+		((countT++));;
+		*);;
+	esac
 done
+if [ $countH -eq $countT ]
+then 
+	echo "ITS TAI"
+elif [ $countH -gt $countT ]
+then
+	echo "HEADS WON......HEADS COUNT:$countH"
+else
+	echo "TAIL WON......TAIL's COUNT:$countT"
+fi 

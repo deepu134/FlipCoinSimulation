f708f512e196f922b83c6ad8dccbbc58537d1430
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-09 22:10:20 +0100

    Added:Entend UC3 minimum 2 diff win is tai

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,20 +1,5 @@
 #!/bin/bash -x
 echo "FLIP COIN SIMULATION PROBLEM"
-read -p "ENTER THE NUMBER OF FLIPS" num
-count=0
-for (( i=0;i<$num;i++ ))
-do
-	rand=$((RANDOM%2))
-	if [ $rand -eq 1 ]
-	then
-		echo "HEADS"
-		((countH++))
-	else
-		echo "TAIL"
-		((countT++))
-	fi
-done
-echo "HEADS COUNT:$countH TAIL COUNT:$countT"
 countH=0
 countT=0
 for ((i=0;i<21;i++))
@@ -28,9 +13,27 @@ do
 		*);;
 	esac
 done
+echo "HEADS :$countH TAIL:$countT"
 if [ $countH -eq $countT ]
 then 
 	echo "ITS TAI"
+	while [ $(($countH-$countT )) -ne 2 ] && [ $(($countT-$countH )) -ne 2 ] 
+	do
+	rand=$((RANDOM%2))
+        case $rand in
+                0)
+                ((countH++));;
+                1)
+                ((countT++));;
+                *);;
+        esac
+	done
+	if [ $countH -gt $countT ]
+	then
+		echo "HEADS WIN...HEADS COUNT:$countH" 
+	else
+	echo "TAIL WIN....TAIL COUNT:$countT"
+	fi
 elif [ $countH -gt $countT ]
 then
 	echo "HEADS WON......HEADS COUNT:$countH"

c7d44d94f23ab58914be6e52b00bd6f184b80aeb
deepak r kulal <deepakrkulal123@gmail.com>
2020-10-09 20:15:43 +0100

    Added:Multiple flip result using loop

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -x
 echo "FLIP COIN SIMULATION PROBLEM"
-rand=$((RANDOM%2))
-if [ $rand -eq 1 ]
-then
-	echo "HEADS"
-else
-	echo "TAIL"
-fi
+read -p "ENTER THE NUMBER OF FLIPS" num
+for (( i=0;i<$num;i++ ))
+do
+	rand=$((RANDOM%2))
+	if [ $rand -eq 1 ]
+	then
+		echo "HEADS"
+	else
+		echo "TAIL"
+	fi
+done
